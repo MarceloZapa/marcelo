20ef57ebe3ba26490238cc50685b9c2035b2b383
MarceloZapa <marcelotodeschinizapalai@gmail.com>
2016-06-21 09:04:44 -0300

    Modificando

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ echo '<h1>Super Projeto</h1>';
 
 echo 'Este e um pojerto quee escrevi tud erado!!';
 echo 'Desse jeito tá parecendo a "da perna peluda" lendo';
-echo 'O certo seria "Esse é um projeto que escrevi tudo errado"';
+echo 'O correto seria "Esse é um projeto que escrevi tudo errado"';
